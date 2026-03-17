dc13f4cf9df09d97de008c2267a7102a3c784afa
huudatit <huudat81415@gmail.com>
2026-03-18 00:59:09 +0700

    Fixed UI for Work container

--- frontend_react/src/container/Work/Work.tsx
+++ frontend_react/src/container/Work/Work.tsx
@@ -59,8 +59,7 @@ const Work = () => {
             >
               {item}
             </div>
-          ),
-        )}
+          ))}
       </div>
 
       <motion.div
@@ -86,7 +85,7 @@ const Work = () => {
                 <a href={work.projectLink} target="_blank" rel="noreferrer">
                   <motion.div
                     whileInView={{ scale: [0, 1] }}
-                    whileHover={{ scale: [1, 0.9] }}
+                    whileHover={{ scale: [1, 0.90] }}
                     transition={{ duration: 0.25 }}
                     className="app__flex"
                   >
@@ -96,7 +95,7 @@ const Work = () => {
                 <a href={work.codeLink} target="_blank" rel="noreferrer">
                   <motion.div
                     whileInView={{ scale: [0, 1] }}
-                    whileHover={{ scale: [1, 0.9] }}
+                    whileHover={{ scale: [1, 0.90] }}
                     transition={{ duration: 0.25 }}
                     className="app__flex"
                   >
@@ -113,9 +112,7 @@ const Work = () => {
               <div className="app__work-tag app__flex">
                 <p className="p-text">{work.tags[0]}</p>
               </div>
-            
             </div>
-
           </div>
         ))}
       </motion.div>
@@ -126,7 +123,7 @@ const Work = () => {
 const WorkAppWrap = AppWrap(
   MotionWrap(Work, "app__works",),
   "work",
-  "app__primarybg")
-;
+  "app__primarybg"
+);
  
 export default WorkAppWrap;
